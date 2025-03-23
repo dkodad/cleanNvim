57cdc003eb26f8e84160e7adaf1838a7a0f693f3
dkodad <frozurnhozurn@gmail.com>
2025-03-23 01:04:03 +0100

    pyright done

--- lua/cerph/options.lua
+++ lua/cerph/options.lua
@@ -1,16 +1,24 @@
 local opt = vim.opt
-opt.clipboard = "unnamedplus"
-vim.opt.guicursor = ""
+
+opt.guicursor = ""
+
+opt.nu = true
+opt.relativenumber = true
+
 opt.shiftwidth = 4
 opt.tabstop = 4
 opt.softtabstop = 4
+opt.expandtab = true
+
+opt.wrap = false
+
+opt.signcolumn = "yes"
+opt.clipboard = "unnamedplus"
 opt.hlsearch = false
 opt.incsearch = true
 opt.termguicolors = true
 opt.colorcolumn = "80"
-opt.wrap = false
 opt.smartindent = true
-opt.expandtab = true
 opt.isfname:append("@-@")
 vim.g.have_nerd_font = true
 vim.opt.updatetime = 50
